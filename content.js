aca24a690fbe989dcdba82d20af0d5106fed0b37
Jose Herrerías <herreriasjose@gmail.com>
2022-08-26 21:04:28 +0200

    Change way to show images in popup

--- content.js
+++ content.js
@@ -1,5 +1,19 @@
 
-const images = document.getElementsByTagName('img');
-const imagesFiltered = [...images].map((image) => { return  { currentSrc: image.currentSrc, height: image.naturalHeight, with: image.naturalWidth }});
 
-console.log('>>> images found:', imagesFiltered);
+
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(sender.tab ?
+            ">>> message received in content:" + JSON.stringify(request) + " from the script:" + sender.tab.url :
+            ">>> message received in content:" + JSON.stringify(request) + " from the extension");
+
+        const images = document.getElementsByTagName('img');
+        const imagesFiltered = [...images].map((image) => { return { currentSrc: image.currentSrc, height: image.naturalHeight, with: image.naturalWidth } });
+
+
+        console.log('>>> images found:', imagesFiltered);
+
+        sendResponse(imagesFiltered);
+    }
+);
--- popup.js
+++ popup.js
@@ -1,2 +1,31 @@
 // popup.js
 
+
+let changeColor = document.getElementById("getUrl");
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+ 
+
+    
+    // Sending message to background
+    chrome.runtime.sendMessage({name: 'sendpopup'}, function(response) {
+        console.log('<<< Response received:', response);
+      });
+
+	
+  // Sending message to content
+
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {name: "sendpopup"}, function(response) {
+          console.log('<<< Response received:', response);
+          if(response != null) {
+            const viewer = document.getElementById('viewer');
+            for(let o of response) {
+                const image = o;
+                console.log('>>> image:', image);
+            }
+          }
+        });
+      });
+
+});
