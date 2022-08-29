4aa92c290f7b92e87b5c8203c348ea1fe5e1a8e4
Jose Herrerías <herreriasjose@gmail.com>
2022-08-29 09:31:29 +0200

    Add a option to open all images in new tabs

--- popup.js
+++ popup.js
@@ -1,13 +1,13 @@
 // popup.js
 
-const analyze = document.getElementById("analyze");
-analyze.addEventListener("click", async () => {
 
+const analyze = document.getElementById("analyze");
+
+analyze.addEventListener("click", async () => {
     // Sending message to background
     chrome.runtime.sendMessage({ name: 'sendpopup' }, function (response) {
         console.log('Response received:', response);
     });
-
 
     // Sending message to content
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -19,12 +19,17 @@ analyze.addEventListener("click", async () => {
                     name = name[name.length - 1];
                     const height = image.height;
                     const width = image.with;
+                    allImagesList.push(image.currentSrc);
                     return `<li><a href=${image.currentSrc} target="_blank">${name} (${height} x ${width})}</a></li>`
                 });
                 const listImages = `<ul>${images.join('')}</ul>`;
                 const ele = document.createElement("span");
                 ele.innerHTML = listImages;
                 viewer.appendChild(ele);
+                const allImagesButton = document.createElement('button');
+                allImagesButton.innerText = 'All images';
+                allImagesButton.onclick = showAllImages;
+                viewer.appendChild(allImagesButton);
             }
         });
     });
@@ -36,7 +41,21 @@ analyze.addEventListener("click", async () => {
 document.addEventListener('click', event => {
     const a = event.target.closest('a[href]');
     if (a) {
-      event.preventDefault();
-      chrome.tabs.create({url: a.href, active: false});
+        event.preventDefault();
+        chrome.tabs.create({ url: a.href, active: false });
     }
-  });
+});
+
+
+// Images
+
+
+const allImagesList = [];
+
+function showAllImages() {
+    if (confirm(`Are you sure you want to open ${allImagesList.length} new tabs?`)){
+    for(let image of allImagesList){
+        chrome.tabs.create({ url: image });
+    }
+}
+}
