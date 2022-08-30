8b1c20bb020af4e5958e89b1f8fa64e6d56de1cc
Jose Herrerías <herreriasjose@gmail.com>
2022-08-30 09:56:01 +0200

    Change background service

--- background.js
+++ background.js
@@ -1,20 +1,13 @@
 // background.js
-let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
-
-
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    const ver = 1;
-    console.log('>>> ver:', ver);
-    console.log('>>> tabId:', tabId);
-    console.log('>>> tab:', tab);
-    console.log('>>> changeInfo:', changeInfo);
-
-});
-
-
+    console.log('Background service loaded')
+   });
+   
+   
+   
+   chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+       console.log('>>> tabId:', tabId);
+       console.log('>>> tab:', tab);
+       console.log('>>> changeInfo:', changeInfo);
+   });
