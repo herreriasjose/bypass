ea6f99b884a9ca986fcc841f2e81234db90a4ff0
Jose Herrerías <herreriasjose@gmail.com>
2022-09-01 09:45:42 +0200

    Change command selection method

--- popup.js
+++ popup.js
@@ -3,38 +3,79 @@
 
 const getimages = document.getElementById("getimages");
 
+const getCommand = document.getElementById("getcommand");
+
+
+getCommand.addEventListener("change", async (a, b) => {
+    console.log('hit! ', getCommand.value)
+    const value =  getCommand.value;
+    switch (value) {
+        case 'getimages':
+            sendMessageToContent("getimages");
+            return;
+        case 'getscreenshot':
+            alert("Not available yet");
+            return;
+        default:
+            return;
+    }
+
+
+});
+
 getimages.addEventListener("click", async () => {
+
+    sendMessageToContent("getimages");
+
+});
+
+
+function sendMessageToBackground(command, parameters = null) {
     // Sending message to background
     // chrome.runtime.sendMessage({ message: 'getimages' }, function (response) {
     //     console.log('Response received:', response);
     // });
 
+}
+
+
+function handleResponse(response) {
+    const display = document.getElementById('display');
+    const images = response.map(image => {
+        let name = image.currentSrc.split('/');
+        name = name[name.length - 1];
+        const height = image.height;
+        const width = image.with;
+        allImagesList.push(image.currentSrc);
+        return `<li><a href=${image.currentSrc} target="_blank">${name} (${height} x ${width})}</a></li>`
+    });
+    const listImages = `<ul>${images.join('')}</ul>`;
+    const ele = document.createElement("span");
+    ele.innerHTML = listImages;
+    display.appendChild(ele);
+    const allImagesButton = document.createElement('button');
+    allImagesButton.innerText = 'All images';
+    allImagesButton.onclick = showAllImages;
+    display.appendChild(allImagesButton);
+}
+
+
+function sendMessageToContent(command, parameters = null) {
     // Sending message to content
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { command: "getimages" }, function (response) {
+        chrome.tabs.sendMessage(tabs[0].id, { command }, function (response) {
             if (response != null) {
-                const display = document.getElementById('display');
-                const images = response.map(image => {
-                    let name = image.currentSrc.split('/');
-                    name = name[name.length - 1];
-                    const height = image.height;
-                    const width = image.with;
-                    allImagesList.push(image.currentSrc);
-                    return `<li><a href=${image.currentSrc} target="_blank">${name} (${height} x ${width})}</a></li>`
-                });
-                const listImages = `<ul>${images.join('')}</ul>`;
-                const ele = document.createElement("span");
-                ele.innerHTML = listImages;
-                display.appendChild(ele);
-                const allImagesButton = document.createElement('button');
-                allImagesButton.innerText = 'All images';
-                allImagesButton.onclick = showAllImages;
-                display.appendChild(allImagesButton);
+                handleResponse(response);
             }
         });
     });
 
-});
+}
+
+function handleSelect(ele) {
+    console.log(el.value);
+}
+
 
 
 // Avoid popup closing after clicking anchor
@@ -53,9 +94,9 @@ document.addEventListener('click', event => {
 const allImagesList = [];
 
 function showAllImages() {
-    if (confirm(`Are you sure you want to open ${allImagesList.length} new tabs?`)){
-    for(let image of allImagesList){
-        chrome.tabs.create({ url: image });
+    if (confirm(`Are you sure you want to open ${allImagesList.length} new tabs?`)) {
+        for (let image of allImagesList) {
+            chrome.tabs.create({ url: image });
+        }
     }
-}
 }
