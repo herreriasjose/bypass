a2ffa176478e17b18894834975aa874f7e0f87ab
Jose Herrerías <herreriasjose@gmail.com>
2022-09-03 08:21:15 +0200

    Change the way to handle commands

--- public/popup.js
+++ public/popup.js
@@ -1,12 +1,9 @@
 // popup.js
 
-
-const getimages = document.getElementById("getimages");
 const getCommand = document.getElementById("getcommand");
 
-
 getCommand.addEventListener("change", async (a, b) => {
-    const value =  getCommand.value;
+    const value = getCommand.value;
     switch (value) {
         case 'getimages':
             sendMessageToContent("getimages");
@@ -20,10 +17,7 @@ getCommand.addEventListener("change", async (a, b) => {
         default:
             return;
     }
-
-
 });
-
 
 function sendMessageToBackground(command, parameters = null) {
     // Sending message to background
@@ -33,40 +27,34 @@ function sendMessageToBackground(command, parameters = null) {
 
 }
 
-
 function handleResponse(response) {
-    const display = document.getElementById('display');
-    const images = response.map(image => {
-        let name = image.currentSrc.split('/');
-        name = name[name.length - 1];
-        const height = image.height;
-        const width = image.with;
-        allImagesList.push(image.currentSrc);
-        return `<li><a href=${image.currentSrc} target="_blank">${name} (${height} x ${width})}</a></li>`
-    });
-    const listImages = `<ul>${images.join('')}</ul>`;
-    const ele = document.createElement("span");
-    ele.innerHTML = listImages;
-    display.appendChild(ele);
-    const allImagesButton = document.createElement('button');
-    allImagesButton.classList.add('form-control');
-    allImagesButton.classList.add('m-2');
-    allImagesButton.innerText = 'All images';
-    allImagesButton.onclick = showAllImages;
-    display.appendChild(allImagesButton);
-}
+    console.log('>>> response:', response);
+    const command = response.command;
+    const data = response.data;
+    if (command != null) {
+        switch (command) {
+            case "getimages":
+                displayImages(data);
+                return;
+            case "getnanoid":
+                console.log('>>> nanoid:', data);
+                return;
+            default:
+                return;
+        }
 
+    }
+}
 
 function sendMessageToContent(command, parameters = null) {
     // Sending message to content
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { command }, function (response) {
+        chrome.tabs.sendMessage(tabs[0].id, { command, parameters }, function (response) {
             if (response != null) {
                 handleResponse(response);
             }
         });
     });
-
 }
 
 // Avoid popup closing after clicking anchor
@@ -79,9 +67,32 @@ document.addEventListener('click', event => {
 });
 
 
-// Images
+// Deal with images
 
 const allImagesList = [];
+
+
+function displayImages(images) {
+    const display = document.getElementById('display');
+    const parsedImages = images.map(image => {
+        let name = image.currentSrc.split('/');
+        name = name[name.length - 1];
+        const height = image.height;
+        const width = image.with;
+        allImagesList.push(image.currentSrc);
+        return `<li><a href=${image.currentSrc} target="_blank">${name} (${height} x ${width})}</a></li>`
+    });
+    const listImages = `<ul>${parsedImages.join('')}</ul>`;
+    const ele = document.createElement("span");
+    ele.innerHTML = listImages;
+    display.appendChild(ele);
+    const allImagesButton = document.createElement('button');
+    allImagesButton.classList.add('form-control');
+    allImagesButton.classList.add('m-2');
+    allImagesButton.innerText = 'All images';
+    allImagesButton.onclick = showAllImages;
+    display.appendChild(allImagesButton);
+}
 
 function showAllImages() {
     if (confirm(`Are you sure you want to open ${allImagesList.length} new tabs?`)) {
--- src/content.ts
+++ src/content.ts
@@ -6,25 +6,24 @@ import {nanoid} from 'nanoid';
 chrome.runtime.onMessage.addListener(
     function (message, sender, sendResponse) {
 
+        const command = message.command;
         let response = null;
-
+        
         console.log(sender.tab ?
             ">>> Message received in content:" + JSON.stringify(message) + " from the script:" + sender.tab.url :
             ">>> Message received in content:" + JSON.stringify(message) + " from the extension");
 
-        if (message != null) {
-            if (message.command === 'getimages') {
+        if (command != null) {
+            if (command === 'getimages') {
                 const images = document.getElementsByTagName('img');
                 const imagesFiltered = [...images].map((image) => { return { currentSrc: image.currentSrc, height: image.naturalHeight, with: image.naturalWidth } });
-                sendResponse(imagesFiltered);
+                response = { command, data: imagesFiltered };
+                sendResponse(response);
             }
-            if (message.command === 'getnanoid') {
+            if (command === 'getnanoid') {
                 const id: string = nanoid();
                 console.log('>>> id:', id);
-                sendResponse(id);
-            }
-            if (message.command === 'sendResponse') {
-                response = 'Hi there!!';
+                response = { command, data: id }
                 sendResponse(response);
             }
         }
